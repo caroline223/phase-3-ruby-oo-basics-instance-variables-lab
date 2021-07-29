550b3b18b372b4c7afcfdeb525d89279c4a9cec5
caroline223 <67647836+caroline223@users.noreply.github.com>
2021-07-28 22:15:14 -0400

    commit tree 4aaca37d065ef4657f18f5c3320e8db523356bc9

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,13 @@
 class Dog
 
+    def name=(dog_name)
+        #instance method to set the dog's name
+        @this_dogs_name = dog_name
+    end
+
+    def name
+        #instance method to get the dog's name
+        @this_dogs_name
+    end
+
 end
